efaa947fc5ff9f7d196c3306e400a21c653ede1b
katelx <katelinx@gmail.com>
2019-12-01 21:19:08 +0800

    2019/1 cleanup

--- 2019/day01/solution.hs
+++ 2019/day01/solution.hs
@@ -1,7 +1,5 @@
 
 f = pred . pred . flip div 3
-r v | v' <= 0 = 0 | otherwise = v' + r v' where v' = f v
+r = sum . tail . takeWhile (> 0) . iterate f
 
-main = let
-  i = map read . words <$> readFile "input"
-  c v = sum . map v <$> i >>= print in c f >> c r
+main = let c v = sum . map v . map read . words <$> readFile "input" >>= print in c f >> c r
